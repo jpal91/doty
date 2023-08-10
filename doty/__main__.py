3021799e88d3de707c10d30a3bc1d96ef7bb5fc5
jpal91 <98477057+jpal91@users.noreply.github.com>
2023-08-10 20:23:28 +0000

    small changes

--- doty/__main__.py
+++ doty/__main__.py
@@ -1,16 +1,17 @@
 import os
-from configparser import ConfigParser
+from configparser import ConfigParser, ExtendedInterpolation
 
 HOME=os.getenv("HOME")
 DOTFILES=os.path.join(HOME, "dotfiles")
 CONFIG=os.path.join(DOTFILES, ".dotyrc")
-dotycfg = ConfigParser(allow_no_value=True)
+dotycfg = ConfigParser(allow_no_value=True, interpolation=ExtendedInterpolation())
 
 def main():
     # TODO: Write some logic in case no config exists
     dotycfg.read(CONFIG)
     sections = dotycfg.sections()
-    print(dotycfg.items('dotyrc'))
+    print(dotycfg['manifest']['multiline'])
+    
 
 if __name__ == "__main__":
     main()
