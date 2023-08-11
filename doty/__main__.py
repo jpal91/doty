78e7541d95a4e4d4b9a2b7f1ffb804e1a676b894
jpal91 <pallanschja@gmail.com>
2023-08-11 07:07:03 -0400

    added symlinks

--- doty/__main__.py
+++ doty/__main__.py
@@ -7,32 +7,44 @@ DOTFILES=os.path.join(HOME, "dotfiles")
 CONFIG=os.path.join(DOTFILES, ".dotyrc")
 dotycfg = ConfigParser(allow_no_value=True, interpolation=ExtendedInterpolation())
 
+def create_symlink(src: str, dst: str):
+    # if os.stat(src).st_size == 0:
+    #     return
+    
+    if os.path.islink(dst):
+        os.unlink(dst)
+    os.symlink(src, dst)
+
 def add_files(files: list, dir_name: str):
+    
     for file in files:
-        path = os.path.join(dir_name, file)
+        dot_path = os.path.join(dir_name, file)
+        home_path = os.path.join(HOME, file)
 
-        if not os.path.isfile(path):
-            home_path = os.path.join(HOME, file)
-
+        if not os.path.isfile(dot_path):
+            
             if os.path.isfile(home_path):
-                shutil.move(home_path, path)
+                shutil.move(home_path, dot_path)
             else:
-                with open(path, "w") as f:
+                with open(dot_path, "w") as f:
                     f.write("")
+        
+        create_symlink(dot_path, home_path)
 
 def main():
+    
     # TODO: Write some logic in case no config exists
     dotycfg.read(CONFIG)
     dir_files = [(fs[0], fs[1].strip().split('\n')) for fs in dotycfg.items('files')]
     
     for df in dir_files:
         name, files = df
-        path = os.path.join(DOTFILES, name)
+        dot_dir = os.path.join(DOTFILES, name)
 
-        if not os.path.isdir(path):
-            os.mkdir(path)
+        if not os.path.isdir(dot_dir):
+            os.mkdir(dot_dir)
         
-        add_files(files, path)
+        add_files(files, dot_dir)
     
 
 if __name__ == "__main__":
