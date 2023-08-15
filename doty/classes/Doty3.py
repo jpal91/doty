3e1e9f74e1152ed5a7243e3bba8871c2ade8d53c
jpal91 <98477057+jpal91@users.noreply.github.com>
2023-08-15 15:17:26 +0000

    made a few alterations

--- doty/classes/Doty3.py
+++ doty/classes/Doty3.py
@@ -9,6 +9,11 @@ import yaml
 HOME = '/tmp/dotytest' # os.environ['HOME']
 DOTDIR = os.path.join(HOME, "dotfiles") # os.environ['DOTDIR']
 
+if os.environ['CODESPACES']:
+    DPATH = '/workspaces/doty'
+else:
+    DPATH = '/home/jpal/dev/doty'
+
 class DotyEntry:
 
     def __init__(self, attribs: dict) -> None:
@@ -16,15 +21,19 @@ class DotyEntry:
         self.src = ''
         self.dst = ''
         self.notes = ''
-        self.broken = False
+        self._broken_entry = False
+        self._valid_link = False
 
         self.__dict__.update(attribs)
         self.complete_entry(attribs)
     
-    def is_broken(self):
-        return self.broken
+    def is_broken_entry(self) -> bool:
+        return self._broken_entry
+    
+    def is_valid_link(self) -> bool:
+        return self._valid_link
 
-    def vals(self):
+    def vals(self) -> dict:
         return {
             'name': self.name,
             'src': self.src,
@@ -32,48 +41,50 @@ class DotyEntry:
             'notes': self.notes,
         }
     
-    def hashable(self):
-        if not self.broken:
+    def hashable(self) -> dict:
+        if not self.is_broken_entry():
             return {
                 'name': self.name,
                 'src': self.src,
                 'dst': self.dst,
             }
-        else:
-            return None
+        
+        return None
 
-    def complete_entry(self, entry: dict):
+    def complete_entry(self, entry: dict) -> None:
         entry = { k.lower(): v for k, v in entry.items() }
 
         if not 'name' in entry:
-            self.broken = True
+            self._broken_entry = True
             return
-        else:
-            self.name = entry['name']
-        
+
+        self.name = os.path.basename(entry['name'])
+
         if 'src' in entry and not os.path.isabs(entry['src']):
             self.src = os.path.join(HOME, entry['src'])
+        elif not 'src' in entry:
+            self.src = os.path.join(HOME, entry['name'])
 
-            if not os.path.exists(self.src):
-                print(f'Invalid entry - {self.src} does not exist')
-                self.broken = True
-                return None
-            
+        if not os.path.exists(self.src):
+            print(f'Invalid entry - {self.src} does not exist')
+            self._broken_entry = True
+            return
+
         if 'dst' in entry and not os.path.isabs(entry['dst']):
             self.dst = os.path.join(DOTDIR, entry['dst'])
-
-        if not 'src' in entry:
-            self.src = os.path.join(HOME, self.name)
-
-            if not os.path.exists(self.src):
-                print(f'Invalid entry - {self.src} does not exist')
-                self.broken = True
-                return None
-            
-        if not 'dst' in entry:
-            self.dst = os.path.join(DOTDIR, self.name)
+        elif not 'dst' in entry:
+            self.dst = os.path.join(DOTDIR, entry['name'])
+        
         if not 'notes' in entry:
             self.notes = ''
+    
+    def check_link(self) -> bool:
+        if not os.path.islink(self.src) or not os.readlink(self.src) == self.dst:
+            self._valid_link = False
+        else:
+            self._valid_link = True
+        
+        return self._valid_link
         
 
 
@@ -94,15 +105,14 @@ class DotyEntries:
                 print(f'Invalid entry - {item}')
                 continue
 
-            if updated_item.is_broken():
+            if updated_item.is_broken_entry():
                 print(f'Cannot parse entry - {item}')
-                continue
 
             new_cfg.append(updated_item)
         
         return new_cfg
-    
-    def get_entries(self) -> list:
+
+    def get_cfg_entries(self) -> list:
         return [ e.vals() for e in self.entries ]
     
     def get_hashable_entries(self) -> list:
@@ -127,8 +137,8 @@ class Doty:
         return self.cfg.get_cfg_hash()
     
     @property
-    def entries(self):
-        return self.cfg.get_entries()
+    def cfg_entries(self):
+        return self.cfg.get_cfg_entries()
     
     def check_cfg_changes(self):
         lock_file = os.path.join(DOTDIR, "doty.lock")
@@ -139,80 +149,10 @@ class Doty:
             lock = f.read()
         
         return lock != self.cfg_hash
-
-    # def hash_entry(self, entry: list):
-    #     entry = entry.copy()
-    #     for e in entry:
-    #         if 'notes' in e:
-    #             del e['notes']
-
-    #     return hashlib.md5(json.dumps(entry, sort_keys=True).encode()).hexdigest()
-    
-    # def complete_entry(self, entry: dict):
-    #     entry = { k.lower(): v for k, v in entry.items() }
-    #     doty_entry = {
-    #         'name': '',
-    #         'src': '',
-    #         'dst': '',
-    #         'notes': '',
-    #     }
-
-    #     if not 'name' in entry:
-    #         print('Invalid entry - missing name')
-    #         return None
-    #     else:
-    #         doty_entry['name'] = entry['name']
-        
-    #     if 'src' in entry and not os.path.isabs(entry['src']):
-    #         doty_entry['src'] = os.path.join(HOME, entry['src'])
-
-    #         if not os.path.exists(entry['src']):
-    #             print(f'Invalid entry - {entry["src"]} does not exist')
-    #             return None
-            
-    #     if 'dst' in entry and not os.path.isabs(entry['dst']):
-    #         doty_entry['dst'] = os.path.join(DOTDIR, entry['dst'])
-
-    #     if not 'src' in entry:
-    #         doty_entry['src'] = os.path.join(HOME, doty_entry['name'])
-
-    #         if not os.path.exists(entry['src']):
-    #             print(f'Invalid entry - {entry["src"]} does not exist')
-    #             return None
-            
-    #     if not 'dst' in entry:
-    #         doty_entry['dst'] = os.path.join(DOTDIR, doty_entry['name'])
-    #     if not 'notes' in entry:
-    #         doty_entry['notes'] = ''
-        
-    #     return doty_entry
-
-    # def check_valid(self):
-    #     new_cfg = []
-
-    #     for item in self.cfg:
-    #         if isinstance(item, str):
-    #             new_item = {
-    #                 'name': item,
-    #             }
-    #             updated_item = self.complete_entry(new_item)
-    #         elif isinstance(item, dict):
-    #             updated_item = self.complete_entry(item)
-    #         else:
-    #             print(f'Invalid entry - {item}')
-    #             continue
-
-    #         if not updated_item:
-    #             print(f'Cannot parse entry - {item}')
-    #             continue
-            
-    #         new_cfg.append(updated_item)
-        
-    #     self.cfg = new_cfg
     
     def write_cfg(self):
-        with open(os.path.join('/home/jpal/dev/doty', "dotycfg.yml"), 'w') as f:
-            yaml.safe_dump(self.entries, f, sort_keys=False)
+        with open(os.path.join(DPATH, "dotycfg.yml"), 'w') as f:
+            yaml.safe_dump(self.cfg_entries, f, sort_keys=False)
 
-        with open(os.path.join('/home/jpal/dev/doty', "doty.lock"), 'w') as f:
+        with open(os.path.join(DPATH, "doty.lock"), 'w') as f:
             f.write(self.cfg_hash)
