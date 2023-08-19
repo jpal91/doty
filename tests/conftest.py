4ac94b7137fa57a2de4c4694dc10a6340618d4b9
jpal91 <pallanschja@gmail.com>
2023-08-19 08:18:34 -0400

    created initial test suite

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+
+def create_dummy_home_files(home_dir):
+    (home_dir / '.bashrc').touch()
+    (home_dir / '.zshrc').touch()
+    (home_dir / '.zsh_history').touch()
+
+def create_dummy_doty_files(doty_dir):
+    (doty_dir / 'dotycfg.yml').touch()
+    (doty_dir / 'doty_lock.yml').touch()
+
+def create_dotyrc(home, doty, cfg, logs):
+    dotyrc = cfg / 'dotyrc'
+    dotyrc.touch()
+
+    envs = [
+        f'DOTHOME="{home}"',
+        f'DOTDIR="{doty}"',
+        f'DPATH="{doty}"',
+        f'DOTY_LOG_PATH="{logs}/doty.log"',
+    ]
+
+    with open(dotyrc, 'w') as f:
+        f.write('\n'.join(envs))
+
+@pytest.fixture(scope='session')
+def temp_dir(tmp_path_factory):
+    home_dir = tmp_path_factory.mktemp('home')
+    dot_dir = home_dir / 'dotfiles'
+    dot_dir.mkdir()
+    cfg_dir = home_dir / '.config' / 'doty'
+    cfg_dir.mkdir(parents=True)
+    log_dir = dot_dir / 'logs'
+    log_dir.mkdir()
+
+    # Create dummy files
+    create_dummy_home_files(home_dir)
+    create_dummy_doty_files(dot_dir)
+    create_dotyrc(home_dir, dot_dir, cfg_dir, log_dir)
+
+    return home_dir
+
---
+++ tests/test_env.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+from dotenv import dotenv_values
+
+def test_home_dir(temp_dir):
+    assert os.path.isdir(temp_dir)
+    assert os.path.isdir(temp_dir / '.config' / 'doty')
+    assert os.path.isfile(temp_dir / '.bashrc')
+    assert os.path.isfile(temp_dir / '.zshrc')
+    assert os.path.isfile(temp_dir / '.zsh_history')
+    assert os.path.isfile(temp_dir / '.config' / 'doty' / 'dotyrc')
+
+def test_dot_dir(temp_dir):
+    assert os.path.isdir(temp_dir / 'dotfiles')
+    assert os.path.isfile(temp_dir / 'dotfiles' / 'dotycfg.yml')
+    assert os.path.isfile(temp_dir / 'dotfiles' / 'doty_lock.yml')
+    assert os.path.isdir(temp_dir / 'dotfiles' / 'logs')
+    assert os.path.isfile(temp_dir / 'dotfiles' / 'logs' / 'doty.log')
+
+def test_env_file(temp_dir):
+    env = dotenv_values(temp_dir / '.config' / 'doty' / 'dotyrc')
+    assert env['DOTHOME'] == str(temp_dir)
+    assert env['DOTDIR'] == str(temp_dir / 'dotfiles')
+    assert env['DPATH'] == str(temp_dir / 'dotfiles')
+    assert env['DOTY_LOG_PATH'] == str(temp_dir / 'dotfiles' / 'logs' / 'doty.log')
